53be49aa7925d252a9af8b9dfeb4622a1cde35ca
ikowalec <kowaleci@cardiff.ac.uk>
2025-11-20 11:13:40 +0000

    more docs

--- codebase/rematch/memory_rematch.py
+++ codebase/rematch/memory_rematch.py
@@ -44,6 +44,7 @@ def filter_similar_structures(db_path,
             # shaved atoms for speed - increase for better accuracy
             keep = shave_slab(row.toatoms(), threshold=3.0, fix=["Ce", "O"])[0]
 
+        # INITIALISE AN ARRAY AND ACCESS ARRAY ELEMENT INSTEAD OF APPENDING
         soap_list.append(get_soap(row.toatoms()[keep]))
 
     keep_indices = []
@@ -95,3 +96,11 @@ def filter_similar_structures(db_path,
 
 keep_indices = filter_similar_structures("codebase/data/prescreened_structures.db")
 print(f"Keeping {len(keep_indices)} unique structures.")
+
+'''MAKE SURE TO SAVE THE KEEP INDICES SOMEWHERE SAFE
+with connect("codebase/data/prescreened_structures.db") as db:
+    with connect("codebase/data/unique_structures.db") as db_out:
+        for i in keep_indices:
+            row = db.get(i + 1)  # ASE DB indices are 1-based
+            db_out.write(row.toatoms())
+'''
